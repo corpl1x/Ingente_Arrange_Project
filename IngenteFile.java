766e4cb88ffded2e87e7dfc171ddfddce3237d32
corpl1x <collestisback@gmail.com>
2024-11-13 09:17:51 +0800

    Add files via upload

---
+++ IngenteFile.java
@@ -0,0 +1,20 @@
+package ingente.file;
+public class IngenteFile 
+{
+    public static void main(String[] args) 
+    {
+        int iNumber = 3;
+        {
+        if (iNumber>=0)
+            System.out.println("First String");
+        }
+        if (iNumber==0)
+        {
+            System.out.println("Second String");
+        }
+        else
+        {
+            System.out.println("Third String");
+        }
+    }
+}
